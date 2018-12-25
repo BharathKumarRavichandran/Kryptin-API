7a4c0296350cd5ee9a3cb25c3d2980c4e126e1e9
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-25 19:44:19 +0530

    Add user status routes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from django.conf.urls import url
+from django.views.generic import RedirectView
+from django.shortcuts import redirect
+from api.views import UserView
 
 urlpatterns = [
-    
+
+    url('user/online/get/', UserView.GetOnlineView.as_view(), name='get-online'),
+    url('user/online/put/', UserView.PutOnlineView.as_view(), name='put-online'),
+
 ]
