e42d82d9457331907ba7bb6227634450c8f8502e
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-25 19:43:44 +0530

    Add model: User

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id  = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, default=None, unique=False)
+    token    = models.CharField(max_length=250, unique=True, null=False)
+    platform = models.CharField(max_length=30, null=False)
+    course   = models.CharField(max_length=200, null=False)
+    status   = models.BooleanField(choices=((True, 'online'), (False, 'offline')))
