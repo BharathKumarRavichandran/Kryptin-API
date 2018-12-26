a97fa7f9a25c312549d85b68304649b93560ea68
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-27 04:47:00 +0530

    Refactor: Update View to consider changes in models

--- api/views/User/UserChatView.py
+++ api/views/User/UserChatView.py
@@ -2,7 +2,7 @@ from django.views import generic
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 
-from api.models import User
+from api.models import User, Course
 from api.helpers.response_helpers import error_response, invalid_params_response
 from api.decorators.response import JsonResponseDecorator
 
--- api/views/User/UserStatusView.py
+++ api/views/User/UserStatusView.py
@@ -2,7 +2,7 @@ from django.views import generic
 from django.views.generic import View
 from django.utils.decorators import method_decorator
 
-from api.models import User
+from api.models import User, Course
 from api.helpers.response_helpers import error_response, invalid_params_response
 from api.decorators.response import JsonResponseDecorator
 
@@ -12,9 +12,9 @@ class GetOnlineView(View):
     def post(self, request):
         
         try:
-            token    = request.POST.get('token')
-            platform = request.POST.get('platform')
-            course   = request.POST.get('course')
+            token      = request.POST.get('token')
+            platform   = request.POST.get('platform')
+            coursename = request.POST.get('coursename')
 
         except KeyError:
             return invalid_params_response("Invalid parameters")
@@ -22,13 +22,13 @@ class GetOnlineView(View):
         status       = True
         online_users = []
 
-        users = User.objects.filter(platform=platform, course=course, status=status).exclude(token=token)
+        users = Course.objects.values('token__username', 'platform','coursename').filter(platform=platform, coursename=coursename, status=status).exclude(token=token)
 
         for user in users:
             online_users.append({
-                'username' : user.username,
-                'platform' : user.platform,
-                'course'   : user.course
+                'username'   : user['token__username'],
+                'platform'   : user['platform'],
+                'coursename' : user['coursename']
             })
 
         return online_users
@@ -40,24 +40,81 @@ class PutOnlineView(View):
     def post(self, request):
 
         try:
-            username = request.POST.get('username')
-            token    = request.POST.get('token')
-            platform = request.POST.get('platform')
-            course   = request.POST.get('course')   
+            username   = request.POST.get('username')
+            token      = request.POST.get('token')
+            platform   = request.POST.get('platform')
+            coursename = request.POST.get('coursename')  
+            status     = True
 
         except KeyError:
             return invalid_params_response("Invalid parameters")
 
+        course = ""
+        user   = ""
+
         try:
-            user = User.objects.get(token=token, platform=platform, course=course)
+            user = User.objects.get(username=username, token=token)
+            print(user)
 
         except User.DoesNotExist:
-            status = True
-            user = User.objects.create(username=username, token=token, platform=platform, course=course, status=status)
-            return "User created and status made online"
+            user = User.objects.create(username=username, token=token)
+            print("User created")
 
-        if user is not None:
-            user.status = True
-            user.save()
+        try:
+            course = Course.objects.get(token=user, platform=platform, coursename=coursename)
+
+        except Course.DoesNotExist:
+
+            if user is not None:
+                course = Course.objects.create(token=user, platform=platform, coursename=coursename, status=status)
+                print("User course created and status made online")
+                return "User course created and status made online"           
+
+        if course is not None:
+            course.status = status
+            course.save()
+            print("User status made online")
             return "User status made online"
             
+
+@method_decorator(JsonResponseDecorator, name='dispatch')
+class PutOfflineView(View):
+    
+    def post(self, request):
+    
+        try:
+            username   = request.POST.get('username')
+            token      = request.POST.get('token')
+            platform   = request.POST.get('platform')
+            coursename = request.POST.get('coursename')  
+            status     = False
+
+        except KeyError:
+            return invalid_params_response("Invalid parameters")
+
+        course = ""
+        user   = ""
+        
+        try:
+            user = User.objects.get(username=username, token=token)
+            print(user)
+
+        except User.DoesNotExist:
+            user = User.objects.create(username=username, token=token)
+            print("User created")
+
+        try:
+            course = Course.objects.get(token=user, platform=platform, coursename=coursename)
+
+        except Course.DoesNotExist:
+
+            if user is not None:
+                course = Course.objects.create(token=user, platform=platform, coursename=coursename, status=status)
+                print("User course created and status made offline")
+                return "User course created and status made offline"            
+
+        if course is not None:
+            course.status = status
+            course.save()
+            print("User status made offline")
+            return "User status made offline"
