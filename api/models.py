16f30cd27d26a94a85ee165ec85b00d4cbf5a1c4
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-27 04:45:05 +0530

    Refactor: Update models with seperate tables for User and Course

--- api/models.py
+++ api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    user_id  = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255, default=None, unique=False)
-    token    = models.CharField(max_length=250, unique=True, null=False)
-    platform = models.CharField(max_length=30, null=False)
-    course   = models.CharField(max_length=200, null=False)
-    status   = models.BooleanField(choices=((True, 'online'), (False, 'offline')))
+    token    = models.CharField(primary_key=True, max_length=255, unique=True, null=False)
+
+class Course(models.Model):
+    course_id  = models.AutoField(primary_key=True)
+    token      = models.ForeignKey(User, db_column='token', on_delete=models.CASCADE)
+    platform   = models.CharField(max_length=30, null=False)
+    coursename = models.CharField(max_length=200, null=False)
+    status     = models.BooleanField(choices=((True, 'online'), (False, 'offline')))
