68e3ce3ef8628953f9e1dfabcb47e76414a226ac
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-29 03:41:00 +0530

    Update routing settings to allow all socket connections to server

--- Kryptin/routing.py
+++ Kryptin/routing.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path, include
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -8,14 +9,10 @@ from api.consumers import ChatConsumer
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
-    'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                [
-                    url(r"^api/user/chat/", ChatConsumer)
-                ]
-            )
+    'websocket': 
+        URLRouter(
+            [
+                path('api/user/chat/', ChatConsumer)
+            ]
         )
-
-    )
 })
