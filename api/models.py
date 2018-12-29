c956254a1b8fdf90952c27bad557d3712a49896e
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-29 13:39:56 +0530

    Modify User model to set username as primary_key

--- api/models.py
+++ api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=255, default=None, unique=False)
-    token    = models.CharField(primary_key=True, max_length=255, unique=True, null=False)
+    username = models.CharField(primary_key=True, max_length=255)
+    token    = models.CharField(max_length=255, unique=True, null=False)
 
 class Course(models.Model):
     course_id  = models.AutoField(primary_key=True)
-    token      = models.ForeignKey(User, db_column='token', on_delete=models.CASCADE)
+    username   = models.ForeignKey(User, db_column='username', on_delete=models.CASCADE)
     platform   = models.CharField(max_length=30, null=False)
     coursename = models.CharField(max_length=200, null=False)
     status     = models.BooleanField(choices=((True, 'online'), (False, 'offline')))
+    
