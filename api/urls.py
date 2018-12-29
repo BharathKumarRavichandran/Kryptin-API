51ea58c1d22fd9e7cd625b464d69995d1899f76c
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-29 19:47:43 +0530

    Add chat-retrieve route to api/urls

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.shortcuts import redirect
-from api.views.User import UserStatusView
+from api.views.User import UserStatusView, UserChatView
 
 app_name = 'api'
 
@@ -12,5 +12,7 @@ urlpatterns = [
 
     url('user/online/get/', UserStatusView.GetOnlineView.as_view(), name='get-online'),
     url('user/online/put/', UserStatusView.PutOnlineView.as_view(), name='put-online'),
+
+    url('user/chat/retrieve', UserChatView.RetrieveChatMessageView.as_view(), name='chat-retrieve')
 
 ]
