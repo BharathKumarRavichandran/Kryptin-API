65fd59a1a0f657ff490eaff92fe4f2f432b72bf9
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2018-12-31 23:19:38 +0530

    Fix: Fix RequiresLoginDecorator

--- api/decorators/requires_login.py
+++ api/decorators/requires_login.py
@@ -16,13 +16,14 @@ def RequiresLoginDecorator(view):
         try:
             request = args[0]
             assert isinstance(request, HttpRequest)
-            user_id = request.session.get('user_id')
-            session_key = request.session.session_key
+
+            user_id      = request.session.get('user_id')
+            session_key  = request.session.session_key
             user_session = Session.objects.get(pk=session_key)
 
             assert user_session.get_decoded().get('user_id') == user_id
 
-            user = User.objects.get(pk=user_id, is_active=True)
+            user = User.objects.get(pk=user_id)
             whitelisted_path = []
 
         except Exception as e:
